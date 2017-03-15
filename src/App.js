2b112a07fde5ea59630afd7943efb07cafe78ad5
Henrik Hartiala <hartialh@student.tut.fi>
2017-03-15 17:56:10 +0200

    React -opettelua ja login logout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,78 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import * as firebase from 'firebase';
 class App extends Component {
+
+  
+  constructor() {
+    super();
+    this.state = {
+      User: 'Loading'
+    };
+  }
+
+  componentDidMount() {
+    // const rootRef = firebase.database().ref().child('user');
+    // const userRef = rootRef.child('9N5BUAM0NxScnSpRQdO9qxhKbqj2');
+    // const nameRef = userRef.child('name');
+    
+    firebase.auth().onAuthStateChanged(firebaseuser => {
+      if(firebaseuser) {
+        var ref = firebase.database().ref('user/'+firebaseuser.uid).child('nick');
+        ref.on('value', result => {
+          this.setState({
+            User: 'Hello ' + result.val(),
+            Login: '',
+            Email: '',
+            Password: '',
+            Logout: <button onClick={this.logout} className="btn btn-primary">Logout</button>
+          });
+        });
+        
+        
+        
+        console.log(firebaseuser.email);
+      } else {
+        this.setState({
+          User: 'Not logged',
+          Email: <input className="form-control" ref={(email) => this.email = email} placeholder="Email"/> ,
+          Password: <input type="password" className="form-control" placeholder="Password" ref={(pw) => this.pw = pw} /> ,
+          Login: <button type="submit" className="btn btn-primary">Login</button>,
+          Logout: ''
+        });  
+      }
+    });
+
+  }
+  logout() {
+    console.log('ulos');
+    firebase.auth().signOut();
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const auth = firebase.auth();
+    const promise = auth.signInWithEmailAndPassword(this.email.value, this.pw.value);
+    promise.catch(e => console.log(e.message));
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Ohsiha</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.User}
         </p>
+        <form onSubmit={this.handleSubmit}>
+          {this.state.Email}
+          {this.state.Password}
+          {this.state.Login}
+          {this.state.Logout}
+        </form>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyDEcJF3o0OGYk1S4aG5Wd17R7qRkrcAvrQ",
+    authDomain: "ohsiha-55ec6.firebaseapp.com",
+    databaseURL: "https://ohsiha-55ec6.firebaseio.com",
+    storageBucket: "ohsiha-55ec6.appspot.com",
+    messagingSenderId: "317737353794"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
